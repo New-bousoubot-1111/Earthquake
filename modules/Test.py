af2b456eae4046e82af1159cf321b2eb660ccb04
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2024-12-20 09:23:18 +0900

    Update Test.py

--- modules/Test.py
+++ modules/Test.py
@@ -95,11 +95,11 @@ class Test(commands.Cog):
             embed = nextcord.Embed(
                 title='地震情報',
                 description=f'''<t:{time}:F>頃{sindo_data["title"]}の地震がありました。''',
-                embed.add_field(name="震源地", value=f"{hypocenter_name}{longitude_and_latitude}", inline=False)
-                embed.add_field(name="マグニチュード", value=hypocenter["magnitude"], inline=False)
-                embed.add_field(name="震源の深さ", value=depth, inline=False)
                 color=sindo_data['color']
             )
+            embed.add_field(name="震源地", value={hypocenter_name}{longitude_and_latitude}, inline=False)
+            embed.add_field(name="マグニチュード", value=hypocenter["magnitude"], inline=False)
+            embed.add_field(name="震源の深さ", value=depth, inline=False)
             embed.set_footer(text='Provided by p2pquake.net')
             channel = self.bot.get_channel(channel_id)
             await channel.send(embed=embed)
